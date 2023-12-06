a05ffc22c9bf5c7b77ca9029327afab4ab4fe66f
macvincent <macvincentaghaoko@gmail.com>
2023-12-05 19:25:27 -0800

    vae specs

--- basicsr/version.py
+++ basicsr/version.py
@@ -1,5 +1,5 @@
 # GENERATED VERSION FILE
-# TIME: Tue Dec  5 19:05:37 2023
-__version__ = '1.2.0+50cb149'
+# TIME: Tue Dec  5 18:32:41 2023
+__version__ = '1.2.0+f0cdcfa'
 short_version = '1.2.0'
 version_info = (1, 2, 0)
